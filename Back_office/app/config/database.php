4fd192f967d3cfd61c4104011471b5b7b79d0460
MamitianaRamp <rampmamitiana@gmail.com>
2026-03-31 12:20:38 +0300

    ty

--- Back_office/app/config/database.php
+++ Back_office/app/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => getenv('DB_HOST') ?: 'db',
+    'host' => getenv('DB_HOST') ?: 'backoffice_db',
     'port' => getenv('DB_PORT') ?: '5432',
     'name' => getenv('DB_NAME') ?: (getenv('POSTGRES_DB') ?: 'backoffice_db'),
     'user' => getenv('DB_USER') ?: (getenv('POSTGRES_USER') ?: 'backoffice_user'),
